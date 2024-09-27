9d531d21eeadac0c3ba9ddfaea7f140a2dc032c4
Mohamed <aitabdesslam.mohamed@gmail.com>
2024-09-27 12:36:25 +0100

    uploading folders and files...

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; <?= date('Y') ?> <a href="https://github.com/AitAbdesslam/">AitAbdesslam</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; <?= date('Y') ?> <a href="https://github.com/AitAbdesslam/">AitAbdesslam</a></strong>
+    </div>
+</footer>
